7e1de12205f3db51bb8c87df8a7a37ac9c63b179
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-09 19:01:56 -0400

    Added actions to add and remove item

---
+++ src/actions/carActions.js
@@ -0,0 +1,9 @@
+export const ADD_ITEM = "ADD_ITEM"
+export const addItem = add => {
+    return { type: ADD_ITEM, payload: add }
+}
+
+export const REMOVE_ITEM = "REMOVE_ITEM"
+export const removeItem = remove => {
+    return { type: REMOVE_ITEM, payload: remove }
+}
