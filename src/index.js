f1345e3a4c3996bdd4a9ccc51225b771d41dde5d
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-09 18:11:45 -0400

    Setted the store and provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { carReducer } from "./reducers";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(carReducer);
+console.log(store);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
